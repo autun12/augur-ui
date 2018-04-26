b92213cf72aafa65d602a01ccb76f6da244fc424
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-04-26 15:42:12 -0700

    Parse and map candle stick data response

--- src/modules/market/actions/load-candlestick-data.js
+++ src/modules/market/actions/load-candlestick-data.js
@@ -1,6 +1,19 @@
 import { augur } from 'services/augurjs'
 import logError from 'utils/log-error'
 
+import { map, mapValues } from 'lodash/fp'
+
+const mutatePeriod = map(({ max, min, start, end, startTimestamp, volume }) => ({
+  period: new Date(startTimestamp * 1000),
+  open: parseFloat(start),
+  close: parseFloat(end),
+  min: parseFloat(min),
+  max: parseFloat(max),
+  volume: parseFloat(volume),
+}))
+
+const mutateOutcome = mapValues(mutatePeriod)
+
 /**
  *
  * @typedef {Object} LoadCandleStickDataOptions
@@ -19,6 +32,8 @@ import logError from 'utils/log-error'
 export const loadCandleStickData = (options = {}, callback = logError) => {
   augur.augurNode.submitRequest('getMarketPriceCandlesticks', options, (err, data) => {
     if (err) return callback(err)
-    callback(null, data)
+
+    const mutatedData = mutateOutcome(data)
+    callback(null, mutatedData)
   })
 }
